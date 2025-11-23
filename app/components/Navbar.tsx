ded1d08c253aec88eb0d3d3796eaacfc4ff95a5a
Gian <gianmagliotti03@gmail.com>
2025-11-23 12:12:56 -0300

    MVP - final fixes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,55 +7,43 @@ import AddChatButton from "./buttons/AddChatButton";
 
 function Navbar() {
     const userId = useUserId();
-    const [copied, setCopied] = useState(false);
+    const [showId, setShowId] = useState(false);
 
-    const copyId = async () => {
-        if (!userId) return;
-        await navigator.clipboard.writeText(userId);
-        setCopied(true);
-        setTimeout(() => setCopied(false), 900);
+    const toggleId = () => {
+        setShowId((prev) => !prev);
     };
 
     return (
         <div className="h-full flex flex-col justify-between items-center py-5">
             <AddChatButton />
 
-            <div className="relative group flex flex-col items-center">
-                
+            <div className="relative flex flex-col items-center">
+
                 {/* AVATAR */}
                 <button
-                    onClick={copyId}
+                    onClick={toggleId}
                     className="rounded-full transition-transform duration-150 active:scale-90 cursor-pointer"
                 >
                     <Avatar name={userId} variant="beam" size={50} />
                 </button>
-                <div
-                    className="
-                        absolute 
-                        left-14 top-1/2 -translate-y-1/2   
-                        px-3 py-1 rounded-xl
-                        bg-gray-200 dark:bg-gray-700
-                        text-xs text-gray-800 dark:text-gray-200
-                        shadow
-                        opacity-0 group-hover:opacity-100
-                        translate-x-[-5px] group-hover:translate-x-0
-                        transition-all duration-150
-                        flex items-center gap-2 pointer-events-none
-                        whitespace-nowrap
-                    "
-                >
-                    <span>{userId}</span>
-                </div>
 
-                {copied && (
-                    <span
+                {/* ID MOSTRADO/OCULTO */}
+                {showId && (
+                    <div
                         className="
-                            absolute -top-7 text-xs text-gray-600 
-                            dark:text-gray-300 opacity-0 animate-fade-in-out
+                            absolute 
+                            left-14 top-1/2 -translate-y-1/2   
+                            px-3 py-1 rounded-xl
+                            bg-gray-200 dark:bg-gray-700
+                            text-xs text-gray-800 dark:text-gray-200
+                            shadow
+                            transition-all duration-150
+                            flex items-center gap-2
+                            whitespace-nowrap
                         "
                     >
-                        Copiado
-                    </span>
+                        <span>{userId}</span>
+                    </div>
                 )}
             </div>
         </div>
